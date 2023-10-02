16af6f6769b3d6e790f707e3e1fa62bf952f01b0
Timoté Ballochi <tballochi@gmail.com>
2023-10-02 10:56:20 +0200

    Projet Puissance 4 Terminé

--- modules/puissance4.js
+++ modules/puissance4.js
@@ -1,6 +1,6 @@
-import {Puissance4} from './main.js';
+import { Puissance4 } from "./main.js";
 
-let P4 = new Puissance4(document.querySelector('.game'), {
-    rows: 6,
-    cols: 7
+let P4 = new Puissance4(document.querySelector(".game"), {
+  rows: 6,
+  cols: 7,
 });
